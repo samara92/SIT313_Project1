7d4d81adc3fda23880de73d13b282d67ecc9cb55
samara92 <spriyana@deakin.edu.au>
2018-08-06 16:40:49 +1000

    Commit

---
+++ Assignment1/Assignment1/Common/Common.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Assignment1.Common
+{
+    public class Common
+    {
+        public static char[] user_submit_answer;
+        public string[] alphabet_characters = {
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+            "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+        };
+    }
+}
---
+++ Assignment1/Assignment1/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace Assignment1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/Theme.AppCompat.Light.NoActionBar", MainLauncher = true)]
+    //[Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+            InitViews();
+
+        }
+
+        // Set our view from the "main" layout resource
+        //SetContentView(Resource.Layout.activity_main);
+
+        private void InitViews()
+        {
+
+        
+            
+        }
+    }
+}
+
