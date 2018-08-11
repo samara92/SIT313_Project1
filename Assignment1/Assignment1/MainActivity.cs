236967100fda1c0fb226d51c92c4158b033b909d
samara92 <spriyana@deakin.edu.au>
2018-08-11 10:35:08 +1000

    Commit

---
+++ Assignment1/Assignment1/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace Assignment1
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true, LaunchMode = Android.Content.PM.LaunchMode.SingleTop, Icon = "@drawable/icon", Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.main);
+
+        }
+    }
+}
+
